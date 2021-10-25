c0bffeb6a7f9be7c5223ca25e91820339d8ca021
Square-Lion <npb630@verizon.net>
2021-10-25 15:06:16 -0400

    Update news.js

--- news.js
+++ news.js
@@ -15,7 +15,7 @@ function appendData(data) {
         if(i % 3 == 0){
             var row = document.createElement("div")
             row.className = "row";
-            mainContainer.appendChild(div);
+            mainContainer.appendChild(row);
         }
         var div = document.createElement("div");
         div.innerHTML = data[i].title;
