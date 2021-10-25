76083a39e465e69c85ee27e4b5e1a69e204d4baf
Square-Lion <npb630@verizon.net>
2021-10-25 14:58:55 -0400

    Update news.js

--- news.js
+++ news.js
@@ -12,6 +12,11 @@ fetch('news.json')
 function appendData(data) {
         var mainContainer = document.getElementById("menu-box");
     for (var i = 0; i < data.length; i++) {
+        if(i % 3 == 0){
+            var row = document.createElement("div")
+            row.className = "row";
+            
+        }
         var div = document.createElement("div");
         div.innerHTML = data[i].title;
         mainContainer.appendChild(div);
