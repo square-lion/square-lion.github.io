6119a136e8475f3e5cc7b52aa2d57327f0d06618
Square-Lion <npb630@verizon.net>
2021-10-25 14:41:27 -0400

    Update news.js

--- news.js
+++ news.js
@@ -1,20 +1,19 @@
-fetch('news.json'){
-	.then(function (responce){
-		return responce.json();
-	})
-	.then(function (data){
-    	appendData(data);
-  	})
-	.catch(function (err){
-		console.log(err);
-	});
-}
+fetch('people.json')
+    .then(function (response) {
+        return response.json();
+})
+.then(function (data) {
+    appendData(data);
+})
+.catch(function (err) {
+    console.log('error: ' + err);
+});
 
 function appendData(data) {
-  var mainContainer = document.getElementById("menu-box");
-  for (var i = 0; i < data.length; i++) {
-    var article = document.createElement("div");
-    div.innerHTML = '<p>' + 
-    mainContainer.appendChild(div);
-  }
+        var mainContainer = document.getElementById("myData");
+    for (var i = 0; i < data.length; i++) {
+        var div = document.createElement("div");
+        div.innerHTML = data[i].title;
+        mainContainer.appendChild(div);
+    }
 }
