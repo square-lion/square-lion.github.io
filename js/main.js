2b98c0ca12335baca7dc7896fffcd045617e261e
Square-Lion <npb630@verizon.net>
2022-01-16 00:08:23 -0500

    maybe mobile

---
+++ js/main.js
@@ -0,0 +1,32 @@
+function switchCSS(windowsize) {
+	  if (windowsize > 800) {
+		$("#switchable").attr("href", "css/desktop.css");
+	  } else {
+		 $("#switchable").attr("href", "css/mobile.css"); 
+	  }
+	}
+
+$(document).ready(function() {
+	switchCSS($(this).width());
+
+	$(window).resize(function() {
+		switchCSS($(this).width());
+		});
+});
+
+
+var ham = document.getElementsByClassName("hamnav-menu")[0];
+var open = true;
+
+function hamClicked(){
+	console.log("I've been called");
+
+	if(!open){
+		ham.style.display = "none";
+		open = true;
+	}
+	else{
+		ham.style.display = "inline";
+		open = false;
+	}
+}
