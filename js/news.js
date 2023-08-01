3df572dc4214b743191ea411d22797e6873a5a2c
Nicholas Buckley <npb630@verizon.net>
2023-08-01 13:51:31 -0400

    News

--- js/news.js
+++ js/news.js
@@ -1,61 +1,19 @@
-fetch('news.json')
+$( document ).ready(function() {
+    fetch('/news.json')
     .then(function (response) {
         return response.json();
-})
-.then(function (data) {
-    appendData(data);
-})
-.catch(function (err) {
-    console.log('error: ' + err);
+    })
+    .then(function (data) {
+        appendData(data);
+    })
+    .catch(function (err) {
+        console.log('error: ' + err);
+    });
 });
 
-function appendData(data) {
-    var mainContainer = document.getElementsByClassName("news-container")[0];
-
+function appendData(data){
     for (var i = 0; i < data.length; i++) {
-        if(i % 4 == 0){
-            row = document.createElement("div")
-            row.className = "row";
-            mainContainer.append(row);
-        }
-
-        var article = document.createElement("div");
-        article.className = "news";
-        article.id = "news-article";
-
-        var title = document.createElement("h4");
-        title.textContent = data[i].title;
-        data[i].title;
-
-        var body = document.createElement("p");
-        body.textContent = data[i].body;
-
-        var date = document.createElement("small")
-        date.textContent = data[i].date;
-
-        if(data[i].hasOwnProperty("image")){
-            var image = document.createElement("img");
-            image.src = data[i].image;
-
-            article.append(image, title, body, date);
-        }
-        else if (data[i].hasOwnProperty("video")){
-            var video = document.createElement("iframe");
-            video.src = data[i].video;
-            video.width = "100%";
-            video.height = "55%";
-            video.title = "YouTube video player";
-            video.frameborder = "0";
-            video.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            video.setAttribute('allowFullScreen', '')
-
-
-
-
-            article.append(video, title, body, date);
-        }
-
-        row.append(article)
+        $("#news-row").append('<div class="col-lg-4 col-md-6 col-sm-12"><div class="card shadow-sm"><img class="img-fluid rounded-top" src="' + data[i].image + '"><div class="card-body"><p class="card-text"><strong>' + data[i].title + '</strong><br>' + data[i].body + '</p><div class="d-flex justify-content-between align-items-center"><button type="button" class="btn btn-sm btn-outline-secondary">View</button><small class="text-body-secondary">' + data[i].date + '</small></div></div></div></div>');
     }
 }
 
